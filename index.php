09ddefba0cac74dad1a787ad2f26617604107cc3
J Maine <33839097+xjmaine@users.noreply.github.com>
2020-07-02 00:38:10 +0000

    Stripe with back-end and transaction view
    Composer needs to be rebuilt in your project directory (check composer page: https://github.com/composer/composer)

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+   
+    <title>Pay Page</title>
+</head>
+<body>
+<script src="https://js.stripe.com/v3/"></script>
+
+<div class="container">
+    <h2 class="my-4 text-center">Intro to Progamming - Module 1 [Ghc50]</h2>
+
+<form action="./charge.php" method="post" id="payment-form">
+      <div class="form-row">
+       <input type="text" name="first_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="First Name">
+       <input type="text" name="last_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="Last Name">
+       <input type="email" name="email" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="Email Address">
+        <div id="card-element" class="form-control">
+          <!-- a Stripe Element will be inserted here. -->
+        </div>
+
+        <!-- Used to display form errors -->
+        <div id="card-errors" role="alert"></div>
+      </div>
+
+      <button>Submit Payment</button>
+    </form>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="./js/charge.js"></script>
+</body>
+</html>
